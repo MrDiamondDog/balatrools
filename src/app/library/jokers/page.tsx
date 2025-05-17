fd835f31a43037b483def90eff85dccbbb9d83b7
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-05-17 15:55:32 -0600

    feat(editor): Added meta editor and finished filling out game data and images

--- src/app/library/jokers/page.tsx
+++ src/app/library/jokers/page.tsx
@@ -1,4 +1,3 @@
-import Joker from "@/components/infocards/Joker";
 import { Balatro } from "@/lib/gamedata";
 
 export default function JokersLibrary() {
@@ -7,9 +6,6 @@ export default function JokersLibrary() {
             <h1 className="text-3xl font-bold mb-2">Jokers</h1>
             <p className="text-gray-600">Every joker in Balatro.</p>
             <div className="flex flex-wrap gap-2 mt-4 overflow-x-hidden overflow-y-scroll h-[80vh]">
-                {Object.keys(Balatro.Joker).map(k => (
-                    <Joker key={k} joker={k} />
-                ))}
             </div>
         </div>
     );
