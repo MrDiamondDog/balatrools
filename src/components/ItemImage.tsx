446630192abbf8bea2fd96fd035c5b47aad00e69
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-05-26 11:40:57 -0600

    Optimizations

--- src/components/ItemImage.tsx
+++ src/components/ItemImage.tsx
@@ -1,10 +1,12 @@
 import { Item } from "@/lib/gamedata";
+import Image from "next/image";
 
 export default function ItemImage({ item, ...props }: { item: Item } & React.ImgHTMLAttributes<HTMLImageElement>) {
     /* é is for séance! */
-    return <img
+    return <Image
         loading="lazy"
         src={`/images/${item.type}/${item.name.replaceAll(" ", "_").replace("é", "_")}.webp`}
         className={props.className}
+        alt={`${item.name} ${item.type}`}
     />;
 }
